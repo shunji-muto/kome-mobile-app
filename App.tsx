2ac3a64007093867d4e6b5f543261c1d693e68b2
shunji-muto <sit.shunji.muto@gmail.com>
2025-06-27 15:12:17 +0900

    joystickコンポーネントの追加

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ReactNativeJoystick } from './component/joystick/joy-stick';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+        <View style={styles.container}>
+          <ReactNativeJoystick radius={50} onMove={console.log} />
+        </View>
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
@@ -16,5 +25,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    borderColor: 'red',
+    borderWidth: 2,
   },
 });
