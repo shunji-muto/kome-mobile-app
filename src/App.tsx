3000c9fbd3314d75dce33b9151624b1e32b98115
shunji-muto <sit.shunji.muto@gmail.com>
2025-07-13 13:13:12 +0900

    feat(joystick): add onMove event handler and improve event data processing
    Refactor styles for better code readability.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { ReactNativeJoystick } from './components/joystick/joy-stick';
+import { IReactNativeJoystickEvent, ReactNativeJoystick } from './components/joystick/joy-stick';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
-  return (
-    <GestureHandlerRootView style={styles.container}>
-      <View style={styles.container}>
-        <View style={styles.container}>
-          <Text>Open up App.tsx to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View>
-        <View style={styles.container}>
-          <ReactNativeJoystick radius={50} onMove={console.log} />
-        </View>
-      </View>
-    </GestureHandlerRootView>
-  );
+	// ジョイスティックのonMoveイベント用ハンドラー
+	const handleJoystickMove = (event: IReactNativeJoystickEvent) => {
+		// 必要に応じてイベントデータを処理
+		console.log('Joystick Event:', event);
+	};
+	return (
+		<GestureHandlerRootView style={styles.container}>
+			<View style={styles.container}>
+				<View style={styles.container}>
+					<Text>Open up App.tsx to start working on your app!</Text>
+					<StatusBar style="auto" />
+				</View>
+				<View style={styles.container}>
+					<ReactNativeJoystick radius={50} onMove={handleJoystickMove} />
+				</View>
+			</View>
+		</GestureHandlerRootView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderColor: 'red',
-    borderWidth: 2,
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderColor: 'red',
+		borderWidth: 2,
+	},
 });
