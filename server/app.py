0b22f9a91062007425753b31cbb67508ec0136c4
shunji-muto <sit.shunji.muto@gmail.com>
2025-07-25 15:46:06 +0900

    update app.py

---
+++ server/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+from Raspbot_Lib import Raspbot
+import time
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+robot = None
+
+
+def initialize_robot():
+    global robot
+    try:
+        robot = Raspbot()
+        print("Initialize")
+        return True
+    except Exception as e:
+        print(f"Errpr:{e}")
+
+
+@app.route('/')
+def index():
+    return render_template('test.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    print('Message received: ' + msg)
+    send(msg, broadcast=True)
+    #robot.Ctrl_BEEP_Switch(1)
+    # robot.Ctrl_Muto(0,50)
+    # time.sleep(1)
+@socketio.on('Muto')
+def control_muto(data):
+    print(data['left'])
+    print(data['right'])
+    # robot.Ctrl_Muto(0,50)
+    # robot.Ctrl_Muto(1,50)
+    # robot.Ctrl_Muto(2,50)
+    # robot.Ctrl_Muto(3,50)
+    
+
+
+
+
+if __name__ == '__main__':
+    if initialize_robot():
+        print("start")
+    else:
+        print("error-desu")
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
